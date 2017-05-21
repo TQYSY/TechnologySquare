f3e9290370e9f501bdb715351bce680bad6bef8f
Lancher <453711091@qq.com>
2017-05-21 16:55:01 +0800

    主页制作完成

--- src/TechnologySquare/Controllers/CartController.cs
+++ src/TechnologySquare/Controllers/CartController.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace TechnologySquare.Controllers
 {
-    public class HomeController : Controller
+    public class CartController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        
-
-        public IActionResult Error()
+        // GET: /<controller>/
+        public IActionResult Cart()
         {
             return View();
         }
