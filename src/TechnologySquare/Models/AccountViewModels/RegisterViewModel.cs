c2891b405e84a61030ddd4e749c03cc763e1d413
Lancher <453711091@qq.com>
2017-06-10 22:56:04 +0800

    解决冲突

--- src/TechnologySquare/Models/AccountViewModels/RegisterViewModel.cs
+++ src/TechnologySquare/Models/AccountViewModels/RegisterViewModel.cs
@@ -58,35 +58,4 @@ namespace TechnologySquare.Models.AccountViewModels
         [Display(Name = "地址")]
         public string Adress { get; set; }
     }
-    public class RegisterModel
-    {
-        [Display(Name = "用户名")]
-        public string UserName { get; set; }
-
-        [Display(Name = "真实姓名")]
-        public string Conname { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "密码")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "确认密码")]
-        [Compare("Password", ErrorMessage = "密码和确认密码不匹配。")]
-        public string ConfirmPassword { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress, ErrorMessage = "Email地址格式不正确.")]
-        [Display(Name = "电子邮箱")]
-        public string Email { get; set; }
-
-        [RegularExpression(@"^1\d{10}$", ErrorMessage = "移动电话号码不符合格式.")]
-        [Display(Name = "移动电话")]
-        public string MobilePhone { get; set; }
-
-        [Display(Name = "地址")]
-        public string Adress { get; set; }
-    }
 }
